7de87973ce118623773c2d3f949e9e785d76efed
fcastellanosp <111034731+fcastellanosp@users.noreply.github.com>
2023-05-14 09:34:55 -0500

    Línea base del proyecto final

---
+++ Proyecto/resources/batch/streamlit.bat
@@ -0,0 +1,10 @@
+@echo off
+cd..
+cd..
+setlocal
+set PROJECTPATH=%cd%
+set PYTHONPATH=%PYTHON3_HOME%;%cd%
+echo "Starting the app at '%PROJECTPATH%\src\front\main.py"
+echo "Enviroment variable '%PYTHONPATH%"
+%PYTHON3_HOME%\python -m streamlit run %PROJECTPATH%\src\front\main.py
+endlocal
---
+++ Proyecto/src/front/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import Definitions
+
+import altair as alt
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+# import plotly.graph_objects as go
+import seaborn as sns
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)

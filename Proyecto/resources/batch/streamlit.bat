ff7e7c0b9cd177b2ab670da44c66269d47618e8d
fcastellanosp <111034731+fcastellanosp@users.noreply.github.com>
2023-05-23 08:52:16 -0500

    Actualización de línea base

--- Proyecto/resources/batch/streamlit.bat
+++ Proyecto/resources/batch/streamlit.bat
@@ -6,5 +6,5 @@ set PROJECTPATH=%cd%
 set PYTHONPATH=%PYTHON3_HOME%;%cd%
 echo "Starting the app at '%PROJECTPATH%\src\front\main.py"
 echo "Enviroment variable '%PYTHONPATH%"
-%PYTHON3_HOME%\python -m streamlit run %PROJECTPATH%\src\front\main.py
+%PYTHON3_HOME%\python -m streamlit run %PROJECTPATH%\main.py
 endlocal
